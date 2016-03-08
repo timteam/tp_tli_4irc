d4ac5539c90b80207d74ca4e9b1888745b6e1c92
valentin <loki74410@gmail.fr>
2016-03-08 10:49:56 +0100

    Commit valentin mise en place smarty

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
- 
+<?php
+define('SMARTY_DIR', 'lib/smarty/libs/');
+require_once(SMARTY_DIR . 'Smarty.class.php');
+$smarty = new Smarty();
+
+$smarty->template_dir = 'templates/';
+$smarty->compile_dir = 'templates_c/';
+$smarty->config_dir = 'configs/';
+$smarty->cache_dir = 'cache/';
+
+if(!empty($_GET)){
+    $firstKey = array_keys($_GET);
+    include "lib/class/$firstKey[0].php";
+}else{
+   $smarty->display('index.tpl');
+} 
