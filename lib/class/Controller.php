58ba8c88e0eb13731e2bee4deacdd1d5027e4abb
valentin <loki74410@gmail.fr>
2016-03-10 16:49:28 +0100

    Commit valentin changement de place smarty

--- lib/class/Controller.php
+++ lib/class/Controller.php
@@ -18,6 +18,15 @@ class Controller {
         include "DAO/PathologieDAO.php";
         $DAO = new PathologieDAO();
         
+        
+require_once(SMARTY_DIR . 'Smarty.class.php');
+$smarty = new Smarty();
+
+$smarty->template_dir = 'templates/';
+$smarty->compile_dir = 'templates_c/';
+$smarty->config_dir = 'configs/';
+$smarty->cache_dir = 'cache/';
+        
         $smarty->assign('argument',$DAO->selectAll());
         $smarty->assign('module','pathologie.tpl');
         $smarty->display('site.tpl');
