1d05d93775cec6fbb5c539842518b471e1ad683b
Antoine <bouquetant@gmail.com>
2016-03-11 13:36:10 +0100

    Rajout du mot clé UNIQUE sur le name et sur l'id

--- bd/addTables.sql
+++ bd/addTables.sql
@@ -1,8 +1,8 @@
 USE acu;
 DROP TABLE IF EXISTS User CASCADE; 
 CREATE TABLE User(
-    idU NUMERIC(6) NOT NULL auto_increment,
-    name VARCHAR(20) NOT NULL,
+    idU NUMERIC(6) NOT NULL auto_increment UNIQUE,
+    name VARCHAR(20) NOT NULL UNIQUE,
     password VARCHAR(20) NOT NULL,
     lastSignIn DATE,
     primary key (idU)
