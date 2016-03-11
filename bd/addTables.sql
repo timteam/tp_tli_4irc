e5f077139c197e3a8ca8776efdf9436590015d8d
Antoine <bouquetant@gmail.com>
2016-03-11 17:04:51 +0100

    pass varchar(255) for crypted password

--- bd/addTables.sql
+++ bd/addTables.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS User CASCADE;
 CREATE TABLE User(
     idU INT NOT NULL auto_increment,
     name VARCHAR(20) NOT NULL UNIQUE,
-    password VARCHAR(20) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     email VARCHAR(50) NOT NULL,
     lastSignIn DATE,
     primary key (idU),
