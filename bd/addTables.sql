4a392f784648fb2cb537f285965c095e4799f231
Antoine <bouquetant@gmail.com>
2016-03-11 15:58:30 +0100

    trunk

--- bd/addTables.sql
+++ bd/addTables.sql
@@ -1,10 +1,12 @@
 USE acu;
 DROP TABLE IF EXISTS User CASCADE; 
 CREATE TABLE User(
-    idU NUMERIC(6) NOT NULL auto_increment UNIQUE,
+    idU INT NOT NULL auto_increment,
     name VARCHAR(20) NOT NULL UNIQUE,
     password VARCHAR(20) NOT NULL,
     email VARCHAR(50) NOT NULL,
     lastSignIn DATE,
-    primary key (idU)
+    primary key (idU),
+    UNIQUE(idU),
+    UNIQUE(name)
 );
