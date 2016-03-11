549dae2872933ba9644b17ff06367d63afdd4fce
valentin <loki74410@gmail.fr>
2016-03-11 10:04:58 +0100

    Merge origin/master
    Conflicts:
    	lib/class/Controller.php

--- templates/site.tpl
+++ templates/site.tpl
@@ -4,8 +4,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Acupunctura - Medecine chinoise millénaire</title>
     <meta content="Acupunctura - Medecine chinoise millénaire" name="description">
-    <link type="text/css" rel="stylesheet" href="/media/css/site.css">
-    <script type="text/javascript" charset="UTF-8" src="/media/js/site.js"></script>
+    <link type="text/css" rel="stylesheet" href="media/css/site.css">
+    <script type="text/javascript" charset="UTF-8" src="media/js/site.js"></script>
   </head>
   <body>
         {include file='header.tpl'}
--- templates_c/9833bb8f56c6794caa9d1a67bbd093cdf038f75b_0.file.site.tpl.php
+++ templates_c/9833bb8f56c6794caa9d1a67bbd093cdf038f75b_0.file.site.tpl.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.29, created on 2016-03-11 07:32:11
+/* Smarty version 3.1.29, created on 2016-03-11 09:02:42
   from "D:\wamp\www\tpPhp\tp_tli_4irc\templates\site.tpl" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.29',
-  'unifunc' => 'content_56e2747b23d567_17448632',
+  'unifunc' => 'content_56e289b21b9946_10642019',
   'file_dependency' => 
   array (
     '9833bb8f56c6794caa9d1a67bbd093cdf038f75b' => 
     array (
       0 => 'D:\\wamp\\www\\tpPhp\\tp_tli_4irc\\templates\\site.tpl',
-      1 => 1457681186,
+      1 => 1457686957,
       2 => 'file',
     ),
   ),
@@ -21,7 +21,7 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
     'file:footer.tpl' => 1,
   ),
 ),false)) {
-function content_56e2747b23d567_17448632 ($_smarty_tpl) {
+function content_56e289b21b9946_10642019 ($_smarty_tpl) {
 ?>
 <!DOCTYPE html>
 <html lang="fr">
