c6a28d5cd234d6a71e5f4777165479b095000a50
Timothée TRONCY <theatre>
2016-04-27 15:04:33 +0200

    commit

--- lib/class/controllers/PathologiesController.php
+++ lib/class/controllers/PathologiesController.php
@@ -12,56 +12,54 @@
  * @author timotheetroncy
  */
 require_once 'Controller.php';
+
 class PathologiesController extends Controller {
 
-    
-    public $listForTable; 
-    
+    public $listForTable;
+
     public function pathologiesActionGet() {
         require "lib/class/DAO/PathologieDAO.php";
-        
+
         $DAO = new PathologieDAO();
         $list = $DAO->selectAllforPathologies();
-        
+
         $allList = array();
-        
-        $allList['liste']= $this->traitement($list['Pathologies']);
-        $allList['Keywords']= $list['Keywords'];
-        $allList['Meridiens']= $list['Meridiens'];
-        $allList['Pathologies']= $list['Pathologies'];
+
+        $allList['liste'] = $this->traitement($list['Pathologies']);
+        $allList['Keywords'] = $list['Keywords'];
+        $allList['Meridiens'] = $list['Meridiens'];
+        $allList['Pathologies'] = $list['Pathologies'];
         $this->listForTable = $allList;
-        $this->executeMethod($allList, 'pathologie.tpl');   
+        $this->executeMethod($allList, 'pathologie.tpl');
     }
-    
-    
-    private function traitement($list){
+
+    private function traitement($list) {
         require "lib/class/DAO/SymptomeDAO.php";
         $DAOS = new SymptomeDAO();
-        
+
         $finalList = array();
         $meridiensList = array();
         $listSymp = array();
         $name = null;
         foreach ($list as $value) {
-            if($name == null){
+            if ($name == null) {
                 $name = $value->nom;
             }
-            if(strcmp($name, $value->nom) != 0){
-                $finalList[$name]= $meridiensList;
+            if (strcmp($name, $value->nom) != 0) {
+                $finalList[$name] = $meridiensList;
                 $name = $value->nom;
                 $meridiensList = array();
             }
-            
+
             $idP = $value->idP;
             $listSymp = $DAOS->selectSymptonesByPatho($idP);
-            if($listSymp != null){
-                $meridiensList[$value->desc]= $listSymp;
+            if ($listSymp != null) {
+                $meridiensList[$value->desc] = $listSymp;
             }
-            
         }
         return $finalList;
     }
-    
+
     /**
      * Pour filtrer la liste des pathos
      * @param type $listeCodeMeridien
@@ -69,59 +67,76 @@ class PathologiesController extends Controller {
      * @param type $listeCaractPatho
      * @param type $listeIdMotCles
      */
-    public function listePathologiesActionGet($listeCodeMeridien, $listeTypePatho, $listeCaractPatho, $listeIdMotCles ){
+    public function listePathologiesActionGet() {
         require "lib/class/DAO/PathologieDAO.php";
+        $requestParametres = $this->getRequestParametres();
+        if (isset($requestParametres['meridien'])){
+        $listeCodeMeridien = $requestParametres['meridien'];
+        }else{
+            $listeCodeMeridien = array();
+        }
+        if(isset($requestParametres['type'])){
+        $listeTypePatho = $requestParametres['type'];
+        }else{
+            $listeTypePatho = array();
+        }
+        if(isset($requestParametres['caracteristique'])){
+            $listeCaractPatho = $requestParametres['caracteristique'];
+        }else{
+            $listeCaractPatho = array();
+        }
+        if(isset($requestParametres['keyword'])){
+            $listeIdMotCles = $requestParametres['keyword'];
+        }else{
+            $listeIdMotCles = array();
+        }
         
         $pathologieDAO = new PathologieDAO();
-        
+
         $argTypePatho = "";
-        
+
         $listType = array();
         $i = 0;
-        foreach($listeTypePatho as $type){
+        
+        foreach ($listeTypePatho as $type) {
+            
             //Si on a sélectionné des caractères on filtre
-            if(count($listeCaractPatho) > 0){
-                
-                foreach($listeCaractPatho as $carac){
+            if (count($listeCaractPatho) > 0) {
+
+                foreach ($listeCaractPatho as $carac) {
                     $listType[$i] = $type . $carac;
                     $i++;
                 }
-                
-            //Sinon on récupère tous les champs.
-            }else{
+
+                //Sinon on récupère tous les champs.
+            } else {
                 $listTemp = $pathologieDAO->findByTypePatho($type);
-                foreach($listTemp as $temp){
+                foreach ($listTemp as $temp) {
                     $listType[$i] = $temp;
                     $i++;
                 }
-            } 
-            
+            }
         }
         $argCodeMeridien = $this->listToString($listeCodeMeridien);
         $argTypePatho = $this->listToString($listType);
         $argIdKeyWords = $this->listToString($listeIdMotCles);
-        
+
         $listFiltred = array();
-        $listFiltred['Keywords']= $this->listForTable['Keywords'];
-        $listFiltred['Meridiens']= $this->listForTable['Meridiens'];
-        $listFiltred['Pathologies']= $this->listForTable['Pathologies'];
-        $listFiltred['liste']= $pathologieDAO->selectPathoByLists($argCodeMeridien,$argTypePatho,$argIdKeyWords);
-        
-        
-        $this->executeMethod($listFiltred, 'pathologie.tpl');   
+        $listFiltred['liste'] = $pathologieDAO->selectPathoByLists($argCodeMeridien, $argTypePatho, $argIdKeyWords);
+
+
+        $this->executeMethod($listFiltred['liste'], 'tableauPathologie.tpl');
     }
-    
-    
-    public function listToString($list){
+
+    public function listToString($list) {
         $listString = "";
-        foreach($code as $list){
-            if(listString == ""){
-                $listString = "'".$code."'";
-            }else{
-                $listString = $listString . ", '".$code."'";
+        foreach ($code as $list) {
+            if (listString == "") {
+                $listString = "'" . $code . "'";
+            } else {
+                $listString = $listString . ", '" . $code . "'";
             }
         }
-        
-        
     }
+
 }
